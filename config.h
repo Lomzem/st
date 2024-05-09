92079d536e535118d4bc6555e2f4fb7510993785
Lawjay Lee <lawjaylee1@icloud.com>
2024-05-09 05:20:57 -0700

    w

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font =
-    "JetBrainsMonoNL Nerd Font,JetBrainsMonoNL NF:style=Regular:size=24";
+    "JetBrainsMonoNL Nerd Font,JetBrainsMonoNL NF:style=Regular:size=18";
 static int borderpx = 2;
 
 /*
